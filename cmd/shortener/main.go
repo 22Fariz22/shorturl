d423bcd5967da6587d9f3d27c0d71afa73e27dec
22fariz22 <fariz08@gmail.com>
2022-10-25 14:34:07 +0300

    add dirs

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"github.com/gorilla/mux"
+	"log"
+)
+
+func main() {
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", CreateShortUrlHandler)
+
+	log.Fatal(":8080", nil)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module 22Fariz22/shorturl
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
