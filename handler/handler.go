6c72d933596b34e168ad2b98bacd70db5130fdac
22fariz22 <fariz08@gmail.com>
2022-11-15 18:15:10 +0300

    divide shortener and request

--- handler/handler.go
+++ handler/handler.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -30,31 +29,35 @@ func NewHandler() *Handler {
 	}
 }
 
-//RequestURL принимает URL
-func (c *CreateShortURLRequest) RequestURL(w http.ResponseWriter, r *http.Request) string {
-	return fmt.Sprintf("здесь будет body с URL")
+func (h *Handler) ShortenURL(bodyStr string) string {
+	var value CreateShortURLRequest
+
+	h.mu.Lock()
+	countStr := strconv.Itoa(h.Count)
+	h.mu.Unlock()
+
+	value.URL = bodyStr
+
+	h.mu.Lock()
+	h.urls[countStr] = value.URL
+	h.Count++
+	h.mu.Unlock()
+
+	return "http://localhost:8080/" + countStr
 }
 
 //CreateShortUrlHandler Эндпоинт POST / принимает в теле запроса строку URL для сокращения
 //и возвращает ответ с кодом 201 и сокращённым URL в виде текстовой строки в теле.
 func (h *Handler) CreateShortURLHandler(w http.ResponseWriter, r *http.Request) {
 	payload, err := io.ReadAll(r.Body)
-	var value CreateShortURLRequest
 
 	if err != nil {
 		log.Printf("error: %s", err)
 	} else {
-		h.mu.Lock()
-		countStr := strconv.Itoa(h.Count)
-		h.mu.Unlock()
-		value.URL = string(payload)
-		h.mu.Lock()
-		h.urls[countStr] = value.URL
-		h.Count++
-		h.mu.Unlock()
+		short := h.ShortenURL(string(payload))
 
 		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte("http://localhost:8080/" + countStr))
+		w.Write([]byte(short))
 	}
 }
 
@@ -70,13 +73,12 @@ func (h *Handler) GetShortURLByIDHandler(w http.ResponseWriter, r *http.Request)
 }
 
 func (h *Handler) CreateShortURLJSON(w http.ResponseWriter, r *http.Request) {
-	countStr := strconv.Itoa(h.Count)
 
 	payload, err := io.ReadAll(r.Body)
-
 	if err != nil {
 		log.Printf("error: %s", err)
 	} else {
+
 		var value CreateShortURLRequest
 
 		if err := json.Unmarshal(payload, &value); err != nil {
@@ -87,16 +89,12 @@ func (h *Handler) CreateShortURLJSON(w http.ResponseWriter, r *http.Request) {
 			Result string `json:"result"`
 		}
 		resp := Resp{
-			Result: "http://localhost:8080/" + countStr,
+			Result: h.ShortenURL(value.URL),
 		}
 		res, err := json.Marshal(resp)
 		if err != nil {
 			panic(err)
 		}
-		h.mu.Lock()
-		h.urls[countStr] = value.URL
-		h.Count++
-		h.mu.Unlock()
 
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusCreated)
