24f49cbbaa128607bb3b539fc4461b963da87885
22fariz22 <fariz08@gmail.com>
2022-11-21 15:47:50 +0300

    add flag parsing

--- handler/config/config.go
+++ handler/config/config.go
@@ -6,10 +6,16 @@ import (
 	"github.com/caarlos0/env/v6"
 )
 
+const (
+	ServerAddress   = "127.0.0.1:8080"
+	BaseURL         = "http://127.0.0.1:8080"
+	FileStoragePath = "storage/events.json"
+)
+
 type Config struct {
 	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"127.0.0.1:8080"`
 	BaseURL         string `env:"BASE_URL" envDefault:"http://127.0.0.1:8080"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"storage/events.json"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH" `
 }
 
 func NewConnectorConfig() *Config {
