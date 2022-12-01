3ff7e3a8af81a1844d4bf7c6b1a1250cb18fc47f
22fariz22 <fariz08@gmail.com>
2022-12-02 00:27:57 +0300

    fix filestoragepath

--- config/config.go
+++ config/config.go
@@ -11,13 +11,13 @@ import (
 const (
 	DefaultServerAddress   = "127.0.0.1:8080"
 	DefaultBaseURL         = "http://127.0.0.1:8080"
-	DefaultFileStoragePath = "misc/events.json"
+	DefaultFileStoragePath = "/misc/events.json"
 )
 
 type Config struct {
 	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"127.0.0.1:8080"`
 	BaseURL         string `env:"BASE_URL" envDefault:"http://127.0.0.1:8080"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"misc/events.json" `
+	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"/misc/events.json" `
 }
 
 func NewConfig() *Config {
