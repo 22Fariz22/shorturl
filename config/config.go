5b5b7012ea98ad130a92c1943bbea1d01b907f55
22fariz22 <fariz08@gmail.com>
2022-12-02 04:30:58 +0300

    fix

--- config/config.go
+++ config/config.go
@@ -1,10 +1,9 @@
 package config
 
 import (
+	"github.com/caarlos0/env/v6"
 	"github.com/spf13/pflag"
 	"log"
-
-	"github.com/caarlos0/env/v6"
 )
 
 const (
@@ -16,7 +15,7 @@ const (
 type Config struct {
 	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"127.0.0.1:8080"`
 	BaseURL         string `env:"BASE_URL" envDefault:"http://127.0.0.1:8080"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH"  envDefault:"events.json"` //
+	FileStoragePath string `env:"FILE_STORAGE_PATH"  ` //envDefault:"events.json"
 }
 
 func NewConfig() *Config {
