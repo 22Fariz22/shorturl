61000001b3d597a8d4479f93581a0e9c1805b00a
22fariz22 <fariz08@gmail.com>
2022-12-09 20:34:32 +0300

    back  env defaults addres

--- config/config.go
+++ config/config.go
@@ -21,8 +21,8 @@ const (
 )
 
 type Config struct {
-	ServerAddress   string `env:"SERVER_ADDRESS" `     //envDefault:"127.0.0.1:8080"
-	BaseURL         string `env:"BASE_URL" `           //envDefault:"http://127.0.0.1:8080"
+	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"127.0.0.1:8080"`
+	BaseURL         string `env:"BASE_URL" envDefault:"http://127.0.0.1:8080"`
 	FileStoragePath string `env:"FILE_STORAGE_PATH"  ` //envDefault:"events.json"
 	SecretKey       []byte
 	Host            string //`env:"HOST" envDefault:"127.0.0.1"`
@@ -36,8 +36,8 @@ type Config struct {
 func NewConfig() *Config {
 	cfg := &Config{}
 
-	pflag.StringVarP(&cfg.ServerAddress, "server", "a", "", "server address")
-	pflag.StringVarP(&cfg.BaseURL, "baseurl", "b", "", "base URL")
+	pflag.StringVarP(&cfg.ServerAddress, "server", "a", DefaultServerAddress, "server address")
+	pflag.StringVarP(&cfg.BaseURL, "baseurl", "b", DefaultBaseURL, "base URL")
 	pflag.StringVarP(&cfg.FileStoragePath, "file", "f", "", "file storage path")
 	pflag.StringVarP(&cfg.DatabaseDSN, "databasedsn", "d", "", "databaseDSN")
 
