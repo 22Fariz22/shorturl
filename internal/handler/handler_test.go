389617f2486a0c178778d26745be2865a170ac7b
22fariz22 <fariz08@gmail.com>
2023-03-20 11:13:23 +0300

    added a little bit test

--- internal/handler/handler_test.go
+++ internal/handler/handler_test.go
@@ -10,10 +10,12 @@ import (
 	"github.com/22Fariz22/shorturl/internal/cookies"
 	"github.com/22Fariz22/shorturl/internal/handler"
 	"github.com/22Fariz22/shorturl/internal/usecase/memory"
+	"github.com/22Fariz22/shorturl/internal/worker"
 	"github.com/stretchr/testify/assert"
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -37,31 +39,96 @@ func TestHandlerCreateShortURLJSON(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
-		name string
-		in   []byte
-		want string
+		name   string
+		in     []byte
+		status int
 	}{
 		{
-			name: "create json",
-			in:   []byte(`{"url":"https://google.ru"}`),
-			want: `{"result":"http://localhost:8080/`,
+			name:   "create json",
+			in:     []byte(`{"url":"https://google.ru"}`),
+			status: 201,
 		},
 	}
-
-	cfg := config.NewConfig()
 
 	for i := range tests {
 		test := tests[i]
 		t.Run(test.name, func(t *testing.T) {
-
 			repo := memory.New()
 
+			secretKey, err := hex.DecodeString("13d6b4dff8f84a10851021ec8608f814570d562c92fe6b5ec4c9f595bcb3234b")
+			if err != nil {
+				log.Fatal(err)
+			}
+
 			req := httptest.NewRequest(http.MethodPost, "/api/shorten", bytes.NewReader(test.in))
-			req.Header.Add("Content-Type", "application/json")
 			w := httptest.NewRecorder()
 
-			handler.NewHandler(repo, cfg, nil).CreateShortURLJSON(w, req)
+			h := &handler.Handler{
+				Repository: repo,
+				Cfg: config.Config{
+					ServerAddress: "localhost:8080",
+					BaseURL:       "http://localhost:8080",
+					SecretKey:     secretKey,
+				},
+				Workers: nil,
+			}
+			h.CreateShortURLJSON(w, req)
 
+		})
+	}
+}
+
+func TestHandlerCreateShortURLJSONExist(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name   string
+		in     []byte
+		status int
+	}{
+		{
+			name:   "create json",
+			in:     []byte(`{"url":"https://google.ru"}`),
+			status: 409,
+		},
+	}
+
+	for i := range tests {
+		test := tests[i]
+		t.Run(test.name, func(t *testing.T) {
+			repo := memory.New()
+
+			secretKey, err := hex.DecodeString("13d6b4dff8f84a10851021ec8608f814570d562c92fe6b5ec4c9f595bcb3234b")
+			if err != nil {
+				log.Fatal(err)
+			}
+			req := httptest.NewRequest(http.MethodPost, "/api/shorten", bytes.NewReader(test.in))
+			w := httptest.NewRecorder()
+
+			h := &handler.Handler{
+				Repository: repo,
+				Cfg: config.Config{
+					ServerAddress: "localhost:8080",
+					BaseURL:       "http://localhost:8080",
+					SecretKey:     secretKey,
+				},
+				Workers: nil,
+			}
+			h.CreateShortURLJSON(w, req)
+
+			req2 := httptest.NewRequest(http.MethodPost, "/api/shorten", bytes.NewReader(test.in))
+			w2 := httptest.NewRecorder()
+
+			h2 := &handler.Handler{
+				Repository: repo,
+				Cfg: config.Config{
+					ServerAddress: "localhost:8080",
+					BaseURL:       "http://localhost:8080",
+					SecretKey:     secretKey,
+				},
+				Workers: nil,
+			}
+			h2.CreateShortURLJSON(w2, req2)
 		})
 	}
 }
@@ -123,47 +190,60 @@ func TestHandler_DeleteHandler(t *testing.T) { //status 400. как исправ
 		status int
 	}{
 		{
-			name:   "test delete",
+			name:   "test delete OK",
 			status: 202,
 		},
+		{
+			name:   "delete status 400",
+			status: 400,
+		},
+	}
+
+	repo := memory.New()
+
+	secretKey, err := hex.DecodeString("13d6b4dff8f84a10851021ec8608f814570d562c92fe6b5ec4c9f595bcb3234b")
+	if err != nil {
+		log.Fatal(err)
+	}
+	worker := worker.NewWorkerPool(repo)
+
+	h := &handler.Handler{
+		Repository: repo,
+		Cfg: config.Config{
+			ServerAddress: "localhost:8080",
+			BaseURL:       "http://localhost:8080",
+			SecretKey:     secretKey,
+		},
+		Workers: worker,
 	}
 	for _, tt := range tests {
-		repo := memory.New()
-
-		secretKey, err := hex.DecodeString("13d6b4dff8f84a10851021ec8608f814570d562c92fe6b5ec4c9f595bcb3234b")
-		if err != nil {
-			log.Fatal(err)
-		}
-
 		t.Run(tt.name, func(t *testing.T) {
-			h := &handler.Handler{
-				Repository: repo,
-				Cfg: config.Config{
-					ServerAddress: "localhost:8080",
-					BaseURL:       "http://localhost:8080",
-					SecretKey:     secretKey,
-				},
-				Workers: nil,
-			}
+			bodyReader := strings.NewReader(`["short"]`)
 
-			//bodyReader := strings.NewReader(`["short"]`)
-
-			req := httptest.NewRequest(http.MethodDelete, "/api/user/urls", nil)
+			req := httptest.NewRequest(http.MethodDelete, "/api/user/urls", bodyReader)
 			w := httptest.NewRecorder()
+
+			cookies.SetCookieHandler(w, req, h.Cfg.SecretKey)
 
 			repo.SaveURL(context.Background(), "short", "https://ya.ru", "7654321") //save before delete
 
 			h.DeleteHandler(w, req)
+		})
+		t.Run(tt.name, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodDelete, "/api/user/urls", nil)
+			w := httptest.NewRecorder()
+
+			cookies.SetCookieHandler(w, req, h.Cfg.SecretKey)
+
+			h.DeleteHandler(w, req)
 
 			fmt.Println("status: ", w.Code)
-
 		})
 	}
 }
 
 func TestHandler_GetAllURL(t *testing.T) { // получилось
 	//bodyReader := strings.NewReader("")
-
 	tests := []struct {
 		name   string
 		status int
@@ -173,6 +253,7 @@ func TestHandler_GetAllURL(t *testing.T) { // получилось
 			status: 200,
 		},
 	}
+
 	for _, tt := range tests {
 		repo := memory.New()
 
@@ -217,25 +298,26 @@ func TestHandler_GetShortURLByIDHandler(t *testing.T) {
 			status: 307,
 		},
 	}
+
+	repo := memory.New()
+
+	secretKey, err := hex.DecodeString("13d6b4dff8f84a10851021ec8608f814570d562c92fe6b5ec4c9f595bcb3234b")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	h := &handler.Handler{
+		Repository: repo,
+		Cfg: config.Config{
+			ServerAddress: "localhost:8080",
+			BaseURL:       "http://localhost:8080",
+			SecretKey:     secretKey,
+		},
+		Workers: nil,
+	}
+
 	for _, tt := range tests {
-		repo := memory.New()
-
-		secretKey, err := hex.DecodeString("13d6b4dff8f84a10851021ec8608f814570d562c92fe6b5ec4c9f595bcb3234b")
-		if err != nil {
-			log.Fatal(err)
-		}
-
 		t.Run(tt.name, func(t *testing.T) {
-			h := &handler.Handler{
-				Repository: repo,
-				Cfg: config.Config{
-					ServerAddress: "localhost:8080",
-					BaseURL:       "http://localhost:8080",
-					SecretKey:     secretKey,
-				},
-				Workers: nil,
-			}
-
 			req := httptest.NewRequest(http.MethodGet, "/short", nil)
 			w := httptest.NewRecorder()
 
@@ -247,7 +329,6 @@ func TestHandler_GetShortURLByIDHandler(t *testing.T) {
 
 			fmt.Println(w.Code)
 			fmt.Println("Location: ", w.Header().Get("Location"))
-
 		})
 	}
 }
